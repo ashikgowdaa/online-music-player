51ae3706c3c83018a19c489708a84dd09a586fae
Ashik <ashik.ananda12@gmail.com>
2023-03-12 12:39:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,138 @@
+let $ = document
+const image = $.getElementById("cover")
+
+const title = $.getElementById("title")
+
+const artist = $.getElementById("artist")
+
+const music = $.querySelector("audio")
+
+const currentTimeEl = $.getElementById("current-time")
+
+const durationEl = $.getElementById("duration")
+
+const progress = $.getElementById("progress")
+
+const progressContainer = $.getElementById("progress-container")
+
+const prevBtn = $.getElementById("prev")
+
+const playBtn = $.getElementById("play")
+
+const nextBtn = $.getElementById("next")
+
+const background = $.getElementById("background")
+
+const songs = [
+	{filepath:"songs/1.mp3",coverPath:"covers/1.jpeg"},
+	{filepath:"songs/2.mp3",coverPath:"covers/2.jpeg"},
+	{filepath:"songs/3.mp3",coverPath:"covers/3.jpeg"},
+	{filepath:"songs/4.mp3",coverPath:"covers/4.jpeg"},
+	{filepath:"songs/5.mp3",coverPath:"covers/5.jpeg"},
+	{filepath:"songs/6.mp3",coverPath:"covers/6.jpeg"},
+	{filepath:"songs/7.mp3",musicName:"Leila Leila",coverPath:"covers/7.jpeg"},
+	{filepath:"songs/8.mp3",coverPath:"covers/8.jpeg"},
+	{filepath:"songs/9.mp3",coverPath:"covers/9.jpeg"},
+	{filepath:"songs/10.mp3",coverPath:"covers/10.jpeg"}
+]
+
+let isLoading = false
+
+function playSong(){
+	isLoading = true
+	playBtn.classList.replace("fa-play", "fa-pause");
+	playBtn.setAttribute("title", "Pause");
+	music.play()
+}
+
+function pauseSong(){
+	isLoading = false
+	playBtn.classList.replace("fa-pause", "fa-play");
+  	playBtn.setAttribute("title", "Play");
+	music.pause()
+}
+
+
+function playToggle(){
+	if (isLoading) {
+		pauseSong()
+	} else {
+		playSong()
+	}
+}
+
+function loadSongs(song){
+	// title.innerHTML = song.musicName
+	// artist.innerHTML = song.artist
+	music.src = song.filepath
+	changeCover(song.coverPath)
+}
+
+function changeCover(cover) {
+	image.classList.remove("active");
+	setTimeout(function(){
+		image.src = cover
+		image.classList.add("active");
+	},100)
+	background.src = cover
+}
+
+let songIndex = 0
+
+function prevSong(){
+	songIndex--
+	if(songIndex < 0) {
+		songIndex = songs.length
+	}
+	loadSongs(songs[songIndex])
+	playSong()
+}
+
+function nextSong(){
+	songIndex++
+	if(songIndex > songs.length - 1) {
+		songIndex = 0
+	}
+	loadSongs(songs[songIndex])
+	playSong()
+}
+
+loadSongs(songs[songIndex])
+
+
+function updateProgressBar(){
+	if(isLoading){
+		const duration = music.duration
+	let currentTime = music.currentTime
+	let progressPercent = (currentTime / duration) * 100
+	progress.style.width = progressPercent + "%"
+	const durationMinutes = Math.floor(duration / 60)
+	let durationSeconds = Math.floor(duration % 60)
+	if (durationSeconds < 10){
+		durationSeconds = "0" + durationSeconds
+	}
+	if (durationSeconds){
+		durationEl.innerHTML = durationMinutes + ":" + durationSeconds
+	}
+	const currentMinutes = Math.floor(currentTime / 60)
+	let currentSeconds = Math.floor(currentTime % 60)
+	if (currentSeconds < 10) {
+		currentSeconds = "0" + currentSeconds
+	}
+	currentTimeEl.innerHTML = currentMinutes + ":" + currentSeconds
+	}
+}
+
+function setProgressBar(e) {
+	const width = this.clientWidth;
+  	const clickX = e.offsetX;
+  	const duration = music.duration;
+  	music.currentTime = (clickX / width) * duration;
+}
+
+playBtn.addEventListener("click",playToggle)
+prevBtn.addEventListener("click",prevSong)
+nextBtn.addEventListener("click",nextSong)
+music.addEventListener("ended", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
